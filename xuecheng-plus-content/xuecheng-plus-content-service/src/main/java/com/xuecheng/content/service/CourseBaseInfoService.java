1cd8ffe5fd81b1d4ecb9b32ae4ba13704cd201f2
NJUPT-CJ <1280676797@qq.com>
2024-05-27 15:25:08 +0800

    Changes

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+
+/**
+ * @description 课程基本信息管理业务接口
+ * @author Mr.M
+ * @date 2022/9/6 21:42
+ * @version 1.0
+**/
+public interface CourseBaseInfoService  {
+    PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+ }
